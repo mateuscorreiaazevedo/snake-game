e79ba48069fe5a25567e6b3280fe4cc5068c21ab
Marcos Azevedo <marcos.workspace@gmail.com>
2022-02-15 09:45:53 -0300

    feat: create game

---
+++ main.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById("snake")
+const context = canvas.getContext("2d")
+const box = 32
+
+const snake = []
+
+snake.push({
+  x: 8 * box,
+  y: 8 * box,
+})
+
+
+let direction = 'right'
+
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+}
+
+
+function createBG() {
+  context.fillStyle = 'white'
+  context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+function createSnake () {
+  snake.forEach(item => {
+    context.fillStyle = 'green'
+    context.fillRect(item.x, item.y, box, box)
+  })
+}
+
+function createFood () {
+  context.fillStyle = 'red'
+  context.fillRect(food.x, food.y, box, box)
+}
+
+
+document.addEventListener('keydown', update)
+
+function update ( event ) {
+  if (event.keyCode == 37 && direction != 'right')  direction = 'left'
+  if (event.keyCode == 38 && direction != 'down')   direction = 'up'
+  if (event.keyCode == 39 && direction != 'left')   direction = 'right'
+  if (event.keyCode == 40 && direction != 'up')     direction = 'down'
+}
+
+function startGame() {
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+  for (i = 1; i < snake.length; i++) {
+      if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+          clearInterval(game);
+          alert('Game Over :(');
+      }
+  }
+
+  createBG();
+  createSnake();
+  createFood();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if( direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  if (snakeX != food.x || snakeY != food.y) {
+      snake.pop(); // pop tira o último elemento da lista
+  } else {
+      food.x = Math.floor(Math.random() * 15 + 1) * box;
+      food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
+
+  let newHead = {
+      x: snakeX,
+      y: snakeY
+  };
+
+  snake.unshift(newHead); // método unshift adiciona o primeiro quadrado da cobrinha
+}
+
+let game = setInterval(startGame, 120);
